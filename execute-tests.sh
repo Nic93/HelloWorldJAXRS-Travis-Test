e2be365c94d2cdb860673ebbcf53179bf1fca7cc
Nicolai Benz <benz.nicolai93@gmail.com>
2018-06-27 00:37:51 +0200

    Sleep added

--- execute-tests.sh
+++ execute-tests.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-echo "Reponse from http://127.0.0.1:8080/root/rest/hellows:"
+echo "Sleeping for 5 seconds to ensure server got up"
+sleep 5
+echo "Sleep finished"
+echo "Requesting http://127.0.0.1:8080/root/rest/hellows:"
 curl http://127.0.0.1:8080/root/rest/hellows
